9505eb78135c5d359b2b76075d4b3af898499847
Ernesto Villa <albium54@yahoo.com>
2025-05-11 15:27:27 -0500

    finished java code

--- numcounter/java.js
+++ numcounter/java.js
@@ -1,3 +1,41 @@
+////////////////////FIRST VERSION///////////
+
+// let num = (document.getElementById("output").textContent = 0);
+
+// function numDecrease() {
+//   num--;
+//   document.getElementById("output").textContent = num;
+// }
+
+// function numIncrease() {
+//   num++;
+//   document.getElementById("output").textContent = num;
+// }
+
+// function Reset() {
+//   num = 0;
+//   document.getElementById("output").textContent = num;
+// }
+
+/////////////SECOND VERSION//////////////////
+let num = 0;
+// globally sets num for 0
+document.getElementById("output").textContent = num;
+// targets output div and inputs a number in it which in
+// our case it is a 0
 function numDecrease() {
-  let num = document.getElementById("output").value;
+  // define a
+  num--;
+  document.getElementById("output").textContent = num;
+  alert(num);
 }
+
+function numIncrease() {
+  num++;
+  document.getElementById("output").textContent = num;
+}
+function Reset() {
+  num = 0;
+  document.getElementById("output").textContent = num;
+}
+// THIS ONE IS BETTER FOR Readability
